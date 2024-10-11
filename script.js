d78aba69b0d654af770ba73fee8ef5e8d2941db6
NOman-underscroe <shubh.hps@gmail.com>
2024-10-11 15:59:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 link.href = 'mickhand.ttf';  // Since it's in the root directory
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    const link = document.createElement('a');
+    link.href = 'mickhand.ttf';  // Correct relative path to the file
+    link.download = 'mickhand.ttf';
+    link.click();
+});
